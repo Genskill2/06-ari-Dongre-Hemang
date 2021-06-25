2bc975b4e44a326e1e0410013230001bf4613411
Dongre-Hemang <84710644+Dongre-Hemang@users.noreply.github.com>
2021-06-25 16:09:50 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -2,7 +2,7 @@
 #include<math.h>
 #include<ctype.h>
 
-char ari(char str[]){
+string ari(string str[]){
     float score=0;
     int length=strlen(str);
     int characters=0,words=1,sentences=1,value=0;
@@ -21,14 +21,14 @@ char ari(char str[]){
     value=4.71*(characters/words)+0.5*(words/sentences)-21.43;
     score=ceil(value);
    
-    char answer[50];
+    string answer="";
     int scoreValue[14]={1,2,3,4,5,6,7,8,9,10,11,12,13,14};
     char gradeLevel[14][50]={"Kindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"};
     
     for(int i=0;i<14;i++){
         if(scoreValue[i]==score){
-            strcpy(gradeLevel[i],answer[]);
+            strcpy(gradeLevel[i][50],answer);
         }
     }
-   return answer[];
+   return answer;
 }
