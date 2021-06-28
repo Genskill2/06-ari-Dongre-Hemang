6fe789c5fa4334766b8a9ef891a1bfef43778c72
Dongre-Hemang <84710644+Dongre-Hemang@users.noreply.github.com>
2021-06-28 16:36:58 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -6,7 +6,7 @@
 string ari(string str){
     float value=0;
     int length=strlen(str);
-    int characters=0,words=1,sentences=1,score=0;
+    int characters=0,words=0,sentences=0,score=0;
     
     for(int i=0;i<=length;i++){
         if(isalnum(str[i])!='0'){
@@ -24,7 +24,7 @@ string ari(string str){
         }
     }
     value=4.71*(characters/words)+0.5*(words/sentences)-21.43;
-    score=ceil(value);
+    score=ceil(value)+1;
    
    if(score==1){
         return "Kindergarten";
